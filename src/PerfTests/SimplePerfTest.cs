7558d353289d9688808fe5f985dcc66775992610
ig-sinicyn <igor.sinicyn@gmail.com>
2016-11-27 12:22:46 +0300

    rerun

--- src/PerfTests/SimplePerfTest.cs
+++ src/PerfTests/SimplePerfTest.cs
@@ -9,9 +9,10 @@ namespace CodeJam.Dogfooding.PerfTests
 {
 	// A perf test class.
 	[Category("PerfTests: NUnit examples")]
+	[CompetitionBurstMode]
 	public class SimplePerfTest
 	{
-		private const int Count = 100;
+		private const int Count = 10000;
 
 		// Perf test runner method.
 		[Test]
@@ -22,15 +23,15 @@ namespace CodeJam.Dogfooding.PerfTests
 		public void Baseline() => Thread.SpinWait(Count);
 
 		// Competition member #1. Should take ~3x more time to run.
-		[CompetitionBenchmark(2.92, 3.04)]
+		[CompetitionBenchmark(2.91, 3.09)]
 		public void SlowerX3() => Thread.SpinWait(3 * Count);
 
 		// Competition member #2. Should take ~5x more time to run.
-		[CompetitionBenchmark(4.87, 5.07)]
+		[CompetitionBenchmark(4.85, 5.15)]
 		public void SlowerX5() => Thread.SpinWait(5 * Count);
 
 		// Competition member #3. Should take ~7x more time to run.
-		[CompetitionBenchmark(6.81, 7.09)]
+		[CompetitionBenchmark(6.79, 7.21)]
 		public void SlowerX7() => Thread.SpinWait(7 * Count);
 	}
 }
